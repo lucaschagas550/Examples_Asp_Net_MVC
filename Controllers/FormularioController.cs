b64c0cc4f3536374d88bcacba73696f140adc41e
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2024-11-01 19:25:40 -0300

    feat: reset de formulario

---
+++ Controllers/FormularioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Examples_MVC.Controllers;
+
+public class FormularioController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View("Index","Teste Nome");
+    }
+}
